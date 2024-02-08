16624df936207bb9bf031572c2980256de836ca6
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-02-08 12:00:40 +0200

    correct some code

--- src/main.js
+++ src/main.js
@@ -4,27 +4,34 @@ const filterButtons = document.querySelector('.filter-buttons');
 const exerciseFiltersList = document.querySelector('.exercise-filters-list');
 const BASE_URL = 'https://energyflow.b.goit.study/api';
 const filterValueDefault = 'Muscles';
+// на div з кнопками вішаємо слухача
 filterButtons.addEventListener('click', filterBtnClick);
 
 async function filterBtnClick(event) {
+  // preventDefault напево нетреба (нема посилань і сабміту)
   event.preventDefault();
+  // тут посилання на ДОМ-ел на який клікнули
   const filterValue = event.target;
+  // дістаємо значення дата-атрибута елемента, на який клацнули
   const qwer = filterValue.dataset.filter;
-  exerciseFiltersList.innerHTML = '';
+  // чому робиш пустим ul при виклику функції?
+  // exerciseFiltersList.innerHTML = '';
   console.log(qwer);
+  // tagName чи nodeName?
   if (event.target.tagName !== 'BUTTON') {
     return;
   }
   try {
+    // передаємо аргументом значення дата атрибута кнопки на яку клікнули
     getExercises(qwer).then(data => {
       console.log(data);
-      markupExercises(data);
+      exerciseFiltersList.innerHTML = markupExercises(data);
     });
   } catch (error) {
     console.log(error);
   }
 }
-
+// по замовчувнню значення фільтра буде 'Muscles'
 async function getExercises(filter = filterValueDefault) {
   try {
     const response = await axios.get(`${BASE_URL}/filters`, {
@@ -39,7 +46,7 @@ async function getExercises(filter = filterValueDefault) {
     console.log(error);
   }
 }
-
+// функція отримує масив об'єктів
 function markupExercises(results) {
   const markup = results
     .map(
@@ -52,5 +59,7 @@ function markupExercises(results) {
       </li>`
     )
     .join('');
-  exerciseFiltersList.insertAdjacentHTML('beforeend', markup);
+  return markup;
+  // треба іннерhtml, щоб при кліку відбувалась заміна розмітки, а не продовження
+  // exerciseFiltersList.insertAdjacentHTML('beforeend', markup);
 }
